aa13f864837233721eef82c0fe845f400bfd0a4e
jessica <86186397+jessMechanic@users.noreply.github.com>
2022-09-12 15:32:21 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleApp2;
+
+namespace ConsoleApp1
+{
+    class write
+    {
+             public static int[] numbers = {1,2, 3, 4, 5, 6, 7, 8, 9, 10};
+       public static void Main(String[] args) {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write( $"\n{numbers[i]}");
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/write2.cs
@@ -0,0 +1,7 @@
+﻿namespace ConsoleApp2
+{
+    public class write2
+    {
+        public String name = "amungus"  ;
+    }
+}
