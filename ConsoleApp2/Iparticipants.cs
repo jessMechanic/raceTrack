b4ff10cdc9face6600a31355cf2c7aa1ddba6445
jessica <86186397+jessMechanic@users.noreply.github.com>
2022-09-12 15:50:55 +0200

    added interfaces

---
+++ ConsoleApp2/Iparticipants.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace model
+{
+    internal enum TeamColors
+    {
+        Red,
+        Green,
+        Yellow,
+        Grey,
+        Blue
+
+    }
+    internal interface IParticipant
+    {
+        public String Name { get; set; }
+        public int Points { get; set; }
+        public IEquipment Equipment { get; set; }
+        public TeamColors TeamColor { get; set; }
+
+    }
+}
